fd7d7e21e4aba4dbc20c764983546c379b6bd471
Alex Gabriel Cardenas Vazquez <alex.cardenas@coppel.com>
2024-06-21 17:52:57 -0700

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig} from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+import {NgxChartsModule}  from '@swimlane/ngx-charts'
+
+const config: SocketIoConfig = {
+  url: environment.wsUrl, options: {transports:['websocket', 'polling']}
+};
+
 
 import { AppComponent } from './app.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
